ccdb3f8cc8066097af09135cfcc1942dc61b61e3
Ruben Moreno <moreno.greenwater@gmail.com>
2025-03-17 22:12:00 -0500

    local completado

--- src/main/java/com/atlantic/proyect/repository/LocalRepo.java
+++ src/main/java/com/atlantic/proyect/repository/LocalRepo.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocalRepo extends GenericRepo<Local, Long> {
+    boolean existsByNombreIgnoreCase(String nombre);
+
+    boolean existsByNombreIgnoreCaseAndIdLocalIsNot(String nombre, Long idLocal);
 }
--- src/main/java/com/atlantic/proyect/service/impl/LocalServiceImpl.java
+++ src/main/java/com/atlantic/proyect/service/impl/LocalServiceImpl.java
@@ -2,11 +2,16 @@ package com.atlantic.proyect.service.impl;
 
 import com.atlantic.proyect.dto.request.create.LocalDtoRequest;
 import com.atlantic.proyect.entity.Local;
+import com.atlantic.proyect.exception.AlreadyEntityExistException;
 import com.atlantic.proyect.repository.LocalRepo;
 import com.atlantic.proyect.repository.GenericRepo;
 import com.atlantic.proyect.service.ILocalService;
 import com.atlantic.proyect.utils.MapperUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class LocalServiceImpl extends CRUDImpl<Local, LocalDtoRequest,Long> implements ILocalService {
@@ -35,5 +40,27 @@ public class LocalServiceImpl extends CRUDImpl<Local, LocalDtoRequest,Long> impl
     @Override
     protected void setId(Local entity, Long aLong) {
         entity.setIdLocal(aLong);
+    }
+
+    @Override
+    @Transactional
+    public LocalDtoRequest save(LocalDtoRequest localDtoRequest) {
+        if (localRepo.existsByNombreIgnoreCase(localDtoRequest.getNombre())) {
+            Map<String,String> errors = new HashMap<>();
+            errors.put("nombre", localDtoRequest.getNombre());
+            throw new AlreadyEntityExistException("local", errors);
+        }
+        return super.save(localDtoRequest);
+    }
+
+    @Override
+    public LocalDtoRequest update(LocalDtoRequest localDtoRequest, Long aLong) {
+        if (localRepo.existsByNombreIgnoreCaseAndIdLocalIsNot(localDtoRequest.getNombre(), aLong)) {
+            Map<String,String> errors = new HashMap<>();
+            errors.put("nombre", localDtoRequest.getNombre());
+            throw new AlreadyEntityExistException("local", errors);
+        }
+
+        return super.update(localDtoRequest, aLong);
     }
 }
